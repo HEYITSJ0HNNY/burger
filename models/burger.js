7f278463ab477aefbd9ac2c872c619eec658b35a
johnnymoctezuma <johnnymocc@gmail.com>
2017-09-27 20:01:18 -0700

    added models file

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orm.js");
+
+var burg = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cd(res);
+        });
+    },
+
+    create: function(cols, vals, cd) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cd) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        })
+    }
+};
+
+module.exports = burger;
