1c1c887accd050e23fbbdc09155fd2eb273fd9e7
johnnymoctezuma <johnnymocc@gmail.com>
2017-09-27 20:05:45 -0700

    added controller

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var burger = require("../models/burger.js");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            cats: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function() {
+        res.redirect("/");
+    });
+});
+
+module.exports = router;
